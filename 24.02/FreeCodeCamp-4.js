e5dccbbe1771e73f7c8e900c969e3b066afb1945
lelka00777 <lelka00777@gmail.com>
2022-02-24 02:12:53 +0300

    FreeCodecamp

---
+++ 24.02/FreeCodeCamp-4.js
@@ -0,0 +1,27 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/counting-cards
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+ if (card == 2 || card == 3 || card == 4 || card ==5 || card ==6) {				
+   	card < 2 <= 6;
+        count += 1;
+  }
+  else if (card == 7 || card == 8 || card == 9) {
+      count += 0;
+  }
+  else if (card == 10 || card == "J" || card == "Q" || card == "K" || card == "A") {
+      count -= 1;
+  }
+  if (count <= 0) {
+  	return String(count) + " Hold";
+  }
+  else {
+  	return String(count) + " Bet";
+  }
+  // return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
